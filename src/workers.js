9e1c7efba4b8f10169325cf70e679dfc45b3c8f2
r g <rgembalik@users.noreply.github.com>
2020-08-07 16:03:13 +0200

    Data loading reafactor and improved fetch parallelism

---
+++ src/workers.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {Array} input 
+ * @param {int} workerLimit 
+ * @param {function} taskFN 
+ */
+module.exports = async function workers(input, workerLimit = 10, taskFN) {
+  let workerIndices = [... (new Array(workerLimit)).keys()]; // an array of 0,1,..,workerLimit - 1
+  let results = [];
+  currentDataIdx = 0;
+  await Promise.all(workerIndices.map(async function(n){
+      while (input.length) {
+        let localDataIdx = currentDataIdx++;
+        results[localDataIdx] = await taskFN(input.shift());
+      }
+  }));
+  return results;
+}
